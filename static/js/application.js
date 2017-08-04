1dc6211f787da471341a5b0a0e4ff85dd36b4b04
Davanon <davanon2014@gmail.com>
2017-08-04 14:26:06 +0530

    First

---
+++ static/js/application.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+
+    var messages_received = [];
+
+    //receive details from server
+    socket.on('newmessage', function(msg) {
+        console.log("Received message" + msg.message);
+        //maintain a list of ten numbers
+        if (messages_received.length >= 10){
+            messages_received.shift()
+        }            
+        messages_received.push(msg.message);
+        messages_string = '';
+        for (var i = 0; i < messages_received.length; i++){
+            messages_string = messages_string + '<p>' + messages_received[i].toString() + '</p>';
+        }
+        $('#log').html(messages_string);
+    });
+
+});
